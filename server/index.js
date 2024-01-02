04299bd1527505b6846db1abe048ddf7d44a8896
unknown <pranavnaik250901@gmail.com>
2024-01-02 19:48:46 +0530

    env added

--- server/index.js
+++ server/index.js
@@ -3,13 +3,15 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const mealsController = require('./controllers/mealsController');
 const ordersController = require('./controllers/orderController');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://pranavnaik:FoodOrder@cluster0.gv3tdem.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
